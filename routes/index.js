5d69e55f9340778721d58aaa9bb62cbc3795d54a
agutierrez72 <agutierrez72@ucmerced.edu>
2020-06-18 18:21:20 -0700

    add sample pages to navigation bar

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,33 @@
 const express = require('express')
 const router = express.Router()
+
 router.get('/', function(req,res) {
   res.render('home')
+})
+
+router.get('/news', function(req,res) {
+  res.render('news')
+})
+
+router.get('/about', function(req,res) {
+  res.render('about')
+})
+
+router.get('/contact', function(req,res) {
+  res.render('contact')
+})
+
+router.post('/', function(req, res) {
+  let useroptions = {}
+  useroptions = {
+    email: req.query.email,
+    password: req.query.password
+  }
+  console.log(req.query.email)
+  console.log(useroptions["password"])
+})
+
+router.get('/profile', function(req, res) {
+  res.render('profile')
 })
 module.exports = router
